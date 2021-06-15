979ec34c9079787060380012c34eca7605404db0
Pittawat <pete.pittawas@gmail.com>
2021-06-15 18:57:04 +0700

    update : add BottomNavigation and Navigation Component

--- app/src/main/java/com/example/newsapiproject/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/newsapiproject/presentation/main/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.newsapiproject.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsapiproject.R
+import com.example.newsapiproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        val navController = navHost.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
